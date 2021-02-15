c2b0cd20129830d250d85f68fb8b4fc01a6031e5
khh <khh@DESKTOP-BV2U66U>
2021-02-16 00:56:21 +0900

    Java_algorithm_shibata4 commit

---
+++ src/a_study_exercise/chap02/CloneArray.java
@@ -0,0 +1,20 @@
+package a_study_exercise.chap02;
+// 배열을 복제합니다.
+
+class CloneArray {
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+		int[] b = a.clone();
+		
+		b[3] = 7;
+		
+		System.out.print("a =");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(" "+a[i]);
+		}
+		System.out.print("\nb =");
+		for (int j = 0; j < b.length; j++) {
+			System.out.print(" "+b[j]);
+		}
+	}
+}
