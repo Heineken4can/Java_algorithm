e9cb8d46d60a47c31bbb8422e97bfe673d62783e
khh <khh@DESKTOP-BV2U66U>
2021-02-26 23:20:34 +0900

    greedy

---
+++ src/c_ndb796/a_basic/a_greedy/A.java
@@ -0,0 +1,18 @@
+package c_ndb796.a_basic.a_greedy;
+
+public class A {
+    public static void main(String[] args) {
+        int n = 1260;
+        int cnt = 0;
+        int[] coinTypes = {500, 100, 50, 10};
+		
+        for (int i = 0; i < 4; i++) {
+            int coin = coinTypes[i];
+            cnt += n / coin;
+            n %= coin;
+        }
+
+        System.out.println(cnt);
+    }
+
+}
