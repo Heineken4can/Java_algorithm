90f19c6773560841f31bc2fb85925f2c87e29b51
khh <khh@DESKTOP-BV2U66U>
2021-02-27 12:50:39 +0900

    ndb796 sort

---
+++ src/exercise/c_ndb796/a_basic/d_sort/B_swap.java
@@ -0,0 +1,15 @@
+package exercise.c_ndb796.a_basic.d_sort;
+
+public class B_swap {
+	public static void main(String[] args) {
+
+		int[] arr = { 3, 5 };
+
+		// 스와프(Swap)
+		int temp = arr[0];
+		arr[0] = arr[1];
+		arr[1] = temp;
+
+		System.out.println(arr[0] + " " + arr[1]);
+	}
+}
