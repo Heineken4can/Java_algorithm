9f52b2b04cee177c2246b214e676f19fb4d4827f
khh <khh@DESKTOP-BV2U66U>
2021-02-27 10:37:36 +0900

    greedy money

--- src/c_ndb796/a_basic/a_greedy/A.java
+++ src/c_ndb796/a_basic/a_greedy/A.java
@@ -2,17 +2,22 @@ package c_ndb796.a_basic.a_greedy;
 
 public class A {
     public static void main(String[] args) {
-        int n = 1260;
-        int cnt = 0;
-        int[] coinTypes = {500, 100, 50, 10};
-		
-        for (int i = 0; i < 4; i++) {
-            int coin = coinTypes[i];
-            cnt += n / coin;
-            n %= coin;
-        }
-
-        System.out.println(cnt);
+       
+    	int money = 1260;
+    	int cnt = 0;
+    	int[] coinTypes = {500,100,50,10};
+    	
+    	for(int i = 0; i < 4; i++) {
+    		int coin = coinTypes[i];
+    		cnt += money / coin;
+    		System.out.println("1111");
+    		System.out.println(cnt);
+    		money %= coin;
+    		System.out.println(money);
+    	}
+    	
+    	System.out.println(cnt);
+    	
     }
 
 }
