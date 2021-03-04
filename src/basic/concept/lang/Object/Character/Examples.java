2cc81194e0d64f1f32f086ecef4429b4887c24e4
khh <hoonhee.kim11@gmail.com>
2021-03-04 11:56:36 +0900

    int 10억

---
+++ src/basic/concept/lang/Object/Character/Examples.java
@@ -0,0 +1,35 @@
+package basic.concept.lang.Object.Character;
+
+public class Examples {
+	 public static void main(String[] args) {
+
+		 	//https://jess2.tistory.com/37
+			System.out.println("\n1.  ============>");			
+			int sum = 0; //숫자의 합을 저장할 변수
+		    String str = "123";    
+		    for(int i=0; i<3; i++) {
+		        sum += (str.charAt(i) - '0'); //string형을 char형으로 문자 하나하나씩 받고 그걸 숫자형으로 변환하여 sum에 더함.
+		    }
+		    System.out.println(sum); //출력결과: 6
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+	 }
+}
