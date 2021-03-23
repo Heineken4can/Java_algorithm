47cff89148658628e30ee51c28d02b9caa5aeb6e
hoonhee <hoonhee.kim11@gmail.com>
2021-03-23 12:59:53 +0900

    string rearrange

--- src/exercise/d_testForRamda256/a_callRecordSummary/C_stringRearrange2.java
+++ src/exercise/d_testForRamda256/a_callRecordSummary/C_stringRearrange2.java
@@ -1,36 +1,39 @@
 package exercise.d_testForRamda256.a_callRecordSummary;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class C_stringRearrange2 {
 
-	public static ArrayList<Character> arrList1 = new ArrayList<Character>();
-	public static String str;
-	public static int num1 = 0;
-
+	public static String testStr;
+	public static ArrayList<Character> arrLiResult = new ArrayList<Character>();
+	public static int numResult = 0;
+	
 	public static void main(String[] args) {
 		// Scanner sc = new Scanner(System.in);
-		str = "AJKDLSI412K4JSJ9D";
-
-		for (int i = 0; i < str.length(); i ++) {
-			if (Character.isDigit(str.charAt(i))) {
-				num1 += str.charAt(i) - '0';
+		
+		testStr = "AJKDLSI412K4JSJ8D";//
+		//ADDIJJJKKLSS19
+		//ADDIJJJKKLSS
+		
+		StringBuilder sbResult = new StringBuilder();
+		for ( int i = 0; i < testStr.length(); i++) {
+			if (Character.isLetter(testStr.charAt(i))) {
+				sbResult.append(testStr.charAt(i));				
 			} else {
-				arrList1.add(str.charAt(i));
+				numResult += testStr.charAt(i) - '0';
 			}
 		}
+		char[] chResult = sbResult.toString().toCharArray();
+		//Character.
+		System.out.println("1: " + chResult.toString());
+		Arrays.sort(chResult);
+		System.out.println("2: " + chResult.toString());
 		
-		Collections.sort(arrList1);
-		
-		for (int i = 0 ; i < arrList1.size(); i++) {
-			System.out.print(arrList1.get(i));
+		for ( int i = 0; i < chResult.length; i++) {
+			System.out.print(chResult[i]);
 		}
-		
-		if (num1 != 0) System.out.print(num1);
-		
-		System.out.println();
-		
 	}
 }
